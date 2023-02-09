1e196ae7910a8ddfbe46a9d31db8c47151ea7590
neotigger <121525065+neotigger@users.noreply.github.com>
2023-02-09 23:36:26 +0100

    Update river2max.py

--- custom_components/ecoflow_cloud/devices/river2max.py
+++ custom_components/ecoflow_cloud/devices/river2max.py
@@ -57,6 +57,11 @@ class River2Max(BaseDevice):
                           lambda value: {"moduleType": 5, "operateType": "acOutCfg",
                                          "params": {"enabled": value, "out_voltage": -1, "out_freq": 255,
                                                     "xboost": 255}}),
+            
+            EnabledEntity(client, "mppt.cfgAcXboost", const.XBOOST_ENABLED,
+                          lambda value: {"moduleType": 5, "operateType": "acOutCfg",
+                                         "params": {"enabled": 255, "out_voltage": -1, "out_freq": 255,
+                                                    "xboost": value}}),
 
             EnabledEntity(client, "pd.carState", const.DC_ENABLED,
                           lambda value: {"moduleType": 5, "operateType": "mpptCar", "params": {"enabled": value}})
