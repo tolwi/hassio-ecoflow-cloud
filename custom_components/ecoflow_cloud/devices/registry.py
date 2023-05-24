95cebd8a3a0e063bcdbe9101aa4951544b413940
tolwi <tolwi@ukr.net>
2023-05-24 14:17:10 +0300

    RIVER_2_PRO: draft (RIVER_2_MAX copy with adjusted charging power)

--- custom_components/ecoflow_cloud/devices/registry.py
+++ custom_components/ecoflow_cloud/devices/registry.py
@@ -3,7 +3,8 @@ from custom_components.ecoflow_cloud.devices import BaseDevice, DiagnosticDevice
 from custom_components.ecoflow_cloud.devices.delta2 import Delta2
 from custom_components.ecoflow_cloud.devices.delta_pro import DeltaPro
 from custom_components.ecoflow_cloud.devices.river2 import River2
-from custom_components.ecoflow_cloud.devices.river2max import River2Max
+from custom_components.ecoflow_cloud.devices.river2_max import River2Max
+from custom_components.ecoflow_cloud.devices.river2_pro import River2Pro
 from custom_components.ecoflow_cloud.devices.river_max import RiverMax
 from custom_components.ecoflow_cloud.devices.river_pro import RiverPro
 from custom_components.ecoflow_cloud.devices.delta_max import DeltaMax
@@ -12,6 +13,7 @@ devices: dict[str, BaseDevice] = {
     EcoflowModel.DELTA_2.name: Delta2(),
     EcoflowModel.RIVER_2.name: River2(),
     EcoflowModel.RIVER_2_MAX.name: River2Max(),
+    EcoflowModel.RIVER_2_PRO.name: River2Pro(),
     EcoflowModel.DELTA_PRO.name: DeltaPro(),
     EcoflowModel.RIVER_MAX.name: RiverMax(),
     EcoflowModel.RIVER_PRO.name: RiverPro(),
