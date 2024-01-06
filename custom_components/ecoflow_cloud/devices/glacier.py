6bcaa480c9cbd54866ef7993165ef4309279ae1e
Tiaan Viljoen <tiaan@qwix.net>
2024-01-06 15:43:03 +0200

    cleanup

--- custom_components/ecoflow_cloud/devices/glacier.py
+++ custom_components/ecoflow_cloud/devices/glacier.py
@@ -66,9 +66,9 @@ class Glacier(BaseDevice):
             SecondsRemainSensorEntity(client, "pd.iceTm", "Ice Time Remain"),
             LevelSensorEntity(client, "pd.icePercent", "Ice Percentage"),
 
-            MiscSensorEntity(client, "pd.iceMkMode", "Ice Make Mode"),  
+            MiscSensorEntity(client, "pd.iceMkMode", "Ice Make Mode"),
 
-            MiscBinarySensorEntity(client,"pd.iceAlert","Ice Alert"),          
+            MiscBinarySensorEntity(client,"pd.iceAlert","Ice Alert"),
             MiscBinarySensorEntity(client,"pd.waterLine","Ice Water Level OK"),   
 
             QuotasStatusSensorEntity(client)      
