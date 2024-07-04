28e53fe47d4be3ee29c22128617dd48698afbf9b
SergiyUKR <fridaynightband@gmail.com>
2024-07-04 16:52:52 +0300

    Update delta_pro.py

--- custom_components/ecoflow_cloud/devices/delta_pro.py
+++ custom_components/ecoflow_cloud/devices/delta_pro.py
@@ -179,7 +179,7 @@ class DeltaPro(BaseDevice):
             EnabledEntity(client, "pd.watthisconfig", const.BP_ENABLED,
                           lambda value, params: {"moduleType": 0, "operateType": "TCP",
                                                  "params": {"id": 94, "isConfig": value,
-                                                            "bpPowerSoc": value * 50),
+                                                            "bpPowerSoc": value * 50,
                                                             "minDsgSoc": 0,
                                                             "maxChgSoc": 0}}),
         ]
