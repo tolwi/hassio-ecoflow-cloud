89f009991d05c8c785943f8897672bf522716725
tolwi <tolwi@ukr.net>
2024-08-25 21:02:41 +0300

    another fix DictSelectEntity

--- custom_components/ecoflow_cloud/select.py
+++ custom_components/ecoflow_cloud/select.py
@@ -20,27 +20,39 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 class DictSelectEntity(BaseSelectEntity):
     _attr_entity_category = EntityCategory.CONFIG
     _attr_available = False
+
     def __init__(self, client: EcoflowApiClient, device: BaseDevice, mqtt_key: str, title: str, options: dict[str, int],
                  command: Callable[[int], dict[str, Any]] | None, enabled: bool = True, auto_enable: bool = False):
         super().__init__(client, device, mqtt_key, title, command, enabled, auto_enable)
-        self.__options_dict = options
-        self._attr_options = list(options.keys())
+        self._options_dict = options
+        self._options = list(options.keys())
+        self._current_option = None
 
     def options_dict(self) -> dict[str, int]:
-        return self.__options_dict
+        return self._options_dict
 
     def _update_value(self, val: Any) -> bool:
         ival = int(val)
-        lval = [k for k, v in self.__options_dict.items() if v == ival]
+        lval = [k for k, v in self._options_dict.items() if v == ival]
         if len(lval) == 1:
-            self._attr_current_option = lval[0]
+            self._current_option = lval[0]
             return True
         else:
             return False
 
-    async def async_select_option(self, option: str):
+    @property
+    def options(self) -> list[str]:
+        """Return available select options."""
+        return self._options
+
+    @property
+    def current_option(self) -> str:
+        """Return current select option."""
+        return self._current_option
+
+    def select_option(self, option: str) -> None:
         if self._command:
-            val = self.__options_dict[option]
+            val = self._options_dict[option]
             self.send_set_message(val, self.command_dict(val))
 
 class TimeoutDictSelectEntity(DictSelectEntity):
